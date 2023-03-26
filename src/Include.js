3f5d868494cd6d2ef079a12495c8702f9699e44b
Axel <axel77g@hotmail.com>
2023-03-26 21:59:44 +0200

    [WARNING] add warning for slot missing or not used

--- src/Include.js
+++ src/Include.js
@@ -25,6 +25,10 @@ export class Include {
     this.findWarningSyntax();
   }
 
+  get slotsTofill() {
+    return this.includeFile.match(regexConf.slotIncludeSide);
+  }
+
   get dirPath() {
     return path.resolve(this.filePath, "..");
   }
@@ -61,7 +65,7 @@ export class Include {
     for (let i = 0; i < linesAfter.length; i++) {
       match = regex.exec(linesAfter[i]);
 
-      if (match) this.slots.push(new Slot(match, match.index));
+      if (match) this.slots.push(new Slot(match, match.index, this));
       else break;
     }
   }
@@ -102,6 +106,15 @@ export class Include {
           ` Syntax ${match[0]} is not supported in ${this.filePath}`
       );
     }
+    if (!this.parent) return;
+    this.slotsTofill?.forEach((slotIdentifier) => {
+      if (!this.slots.some((slot) => slot.identifier === slotIdentifier)) {
+        console.log(
+          apply(["bg.yellow", "bright"], " WARNING ") +
+            ` Slot ${slotIdentifier} is not filled in ${this.filePath} in ${this.parent.filePath}`
+        );
+      }
+    });
   }
 
   include() {
@@ -122,13 +135,27 @@ export class Include {
 }
 
 export class Slot {
-  constructor([_, leadingSpace, slotIdentifier, value], index) {
+  constructor([_, leadingSpace, slotIdentifier, value], index, include) {
+    /**
+     * @type {Include}
+     */
+    this.include = include;
     this._ = _;
     this.index = index;
     this.identifier = slotIdentifier;
     this.value = value;
     this.leadingSpace = leadingSpace;
     this.slots = [];
+
+    if (!this.isUse())
+      console.log(
+        apply(["bg.yellow", "bright"], " WARNING ") +
+          ` Slot ${this.identifier} is not used in ${this.include.filePath}`
+      );
+  }
+
+  isUse() {
+    return this.include.includeFile.indexOf(this.identifier) > -1;
   }
 
   exec(data) {
--- src/config/regex.js
+++ src/config/regex.js
@@ -2,6 +2,7 @@ export default {
   simpleInclude: /#@include (.+)/gi,
   includeRegex: /([ ]{0,})#@include (.+)/gi,
   slotRegex: /([ ]{0,})(#@slot:.+?)[ ](.+)/gi,
+  slotIncludeSide: /#@slot:\w+$/gm,
   warningSyntax: /#@.+/gi,
   supportedSyntax: /[ ]{0,}#@(include|slot).+/gi,
 };
